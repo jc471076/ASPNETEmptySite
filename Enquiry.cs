4b5944a4220a93f574121e59ffb942527ed9842b
Mayurersh Lande <31034277+jc471076@users.noreply.github.com>
2019-11-02 20:36:35 +1000

    Add files via upload

---
+++ Enquiry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NextGenSoft
+{
+    public class Enquiry
+    {
+      //  public long EnquiryId;
+        public string Name;
+        public string Email;
+        public string PhoneNo;
+        public string Description;
+       /* public bool IsActive;
+        public DateTime CreatedDate;*/
+
+
+    }
+}
---
+++ index.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Services;
+using System.Web.Script.Services;
+
+namespace NextGenSoft
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        [ScriptMethod]
+        public static void SaveEnquiry(Enquiry enquiry)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO m_Enquiry VALUES(@Name, @Email,@PhoneNo,@Description,@IsActive,@CreatedDate)"))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@Name", enquiry.Name);
+                    cmd.Parameters.AddWithValue("@Email", enquiry.Email);
+                    cmd.Parameters.AddWithValue("@PhoneNo", enquiry.PhoneNo);
+                    cmd.Parameters.AddWithValue("@Description", enquiry.Description);
+                    cmd.Parameters.AddWithValue("@IsActive", true);
+                    cmd.Parameters.AddWithValue("@CreatedDate", "");
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+    }
+}
